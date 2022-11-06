2f54dfa51da017e9839c0378ee18d00d25f7aaae
vinitrrane <vinitrrane@gmail.com>
2022-11-06 17:50:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
+import cats from './img/catfact.png'
+
 
 function App() {
+
+  const [catFact, setCatFact] = useState();
+
+  const handleClick = () => {
+    Axios.get('https://catfact.ninja/fact').then((res) => {
+      setCatFact(res.data.fact)
+    })
+  }
+
+  useEffect(() => {
+
+    handleClick();
+    // eslint-disable-next-line
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App '>
+      <div className='btnArea'>
+        <h1>Cat Fact Generator</h1>
+        <p>Generate a random fact by clicking the button</p>
+        <button onClick={handleClick}>Generate a fact</button>
+      </div>
+
+      <div className='factArea'>
+        <p>{catFact}</p>
+        <img src={cats} alt='sorry' />
+      </div>
+
     </div>
-  );
+  )
+
 }
 
 export default App;
+
